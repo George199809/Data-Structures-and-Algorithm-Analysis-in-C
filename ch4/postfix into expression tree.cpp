61de825d32f918a572313b1b69021c2b741c6f93
George199809 <32381088+George199809@users.noreply.github.com>
2018-04-24 01:52:25 +0800

    Summary

---
+++ ch4/postfix into expression tree.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stack>
+using namespace std;
+
+typedef char element_type;
+struct tree_node;
+typedef struct tree_node *ptr_to_node;
+typedef ptr_to_node tree;
+
+struct tree_node { //析构函数失效
+	element_type element;
+	tree left;
+	tree right;
+};
+
+
+int legal(char ch) {
+	if(ch >=48 && ch <=57)
+		return 1;
+	if(ch == '+' || ch == '-' || ch == '*' || ch == '/')
+		return 1;
+	return 0;
+}
+void print(tree p) {
+	if(p->left != NULL) {
+		print(p->left);
+	}
+	printf("%c", p->element);
+	if(p->right != NULL)
+		print(p->right);
+}
+int main() {
+	char ch;
+	stack<tree> s;
+	tree temp;
+	while((ch=getchar()) != EOF) { 
+		if(!legal(ch))
+			continue;
+		temp = (tree_node *) malloc(sizeof(struct tree_node));
+		temp->left = NULL;
+		temp->right = NULL;
+		temp->element = ch;
+		if(ch < 48 || ch > 57) {
+			tree t2 = s.top(); s.pop();
+			tree t1 = s.top(); s.pop();
+			temp->left = t1;
+			temp->right = t2;
+		}
+		s.push(temp);
+	}
+
+	tree p = s.top();
+	print(p);
+	return 0;
+}
